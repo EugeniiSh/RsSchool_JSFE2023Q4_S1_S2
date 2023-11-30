02d62d1bbc55baeba77957a1c6ea785827b72284
EugeniiSh <evgenii.shmerko@mail.ru>
2023-11-30 16:00:27 +0300

    feat: add burger JS, enjoy video

---
+++ coffee-house/sctipts/main.js
@@ -0,0 +1,42 @@
+/* ============================== +Burger menu+ ============================== */
+const burgerButtonConteiner = document.querySelector('.heading__burger-button-conteiner');
+const headingNavContainer = document.querySelector('.heading__nav-container');
+const burgerButtonLine = document.querySelector('.burger-button__line');
+const listItem = document.querySelectorAll('.list-item');
+
+const heading = document.querySelector('.heading');
+const offer = document.querySelector('.offer');
+
+
+burgerButtonConteiner.addEventListener('click', () =>
+{
+    headingNavContainer.classList.toggle('activ-burger');
+    burgerButtonLine.classList.toggle('activ-burger__line');
+    heading.classList.toggle('active-heading');
+    offer.classList.toggle('active-offer');
+});
+
+listItem.forEach((item) =>
+{
+    item.addEventListener('click', () =>
+    {
+        headingNavContainer.classList.remove('activ-burger');
+        burgerButtonLine.classList.remove('activ-burger__line');
+        heading.classList.remove('active-heading');
+        offer.classList.remove('active-offer');
+    });
+});
+
+window.addEventListener('resize',() =>
+{
+    const width = document.body.clientWidth;
+    if(width > 768)
+    {
+        headingNavContainer.classList.remove('activ-burger');
+        burgerButtonLine.classList.remove('activ-burger__line');
+        heading.classList.remove('active-heading');
+        offer.classList.remove('active-offer');
+    }
+});
+/* ============================== -Burger menu- ============================== */
+
