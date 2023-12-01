63596e464f3dac952fec2b68455de8acebf5d9e3
EugeniiSh <evgenii.shmerko@mail.ru>
2023-12-01 19:09:02 +0300

    feat: add all slaider animation

--- coffee-house/sctipts/main.js
+++ coffee-house/sctipts/main.js
@@ -41,6 +41,7 @@ window.addEventListener('resize',() =>
 /* ============================== -Burger menu- ============================== */
 
 /* ============================ +Favorite Carusel+ ============================= */
+const carouselWrapper = document.querySelector('.carousel-wrapper');
 const carouselBlocks = document.querySelector('.carousel-blocks');
 const carouselArrowLeft = document.querySelector('.carousel-arrow-left');
 const carouselArrowRight = document.querySelector('.carousel-arrow-right');
@@ -49,11 +50,16 @@ const paginationBlockItem = document.querySelectorAll('.pagination-block__item')
 let position = 0;
 let pagIndex = 0;
 
+activatePagination();
+console.log(carouselWrapper.offsetWidth);
+
 function nextSlide()
 {
-    if(position < ((paginationBlockItem.length - 1) * 580))
+    const scrollLength = carouselWrapper.offsetWidth + 100;
+
+    if(position < ((paginationBlockItem.length - 1) * scrollLength))
     {
-        position += 580;
+        position += scrollLength;
         pagIndex++;
     }
     else
@@ -63,33 +69,106 @@ function nextSlide()
     }
 
     carouselBlocks.style.left = -position + 'px';
+    activatePagination();
     // thisSlide(pagIndex);
 }
 
 function prevSlide()
 {
+    const scrollLength = carouselWrapper.offsetWidth + 100;
+
     if(position > 0)
     {
-        position -= 580;
+        position -= scrollLength;
         pagIndex--;
     }
     else
     {
-        position = (paginationBlockItem.length - 1) * 580;
-        pagIndex = paginationBlockItem.length;
+        position = (paginationBlockItem.length - 1) * scrollLength;
+        pagIndex = paginationBlockItem.length - 1;
     }
 
     carouselBlocks.style.left = -position + 'px';
+    activatePagination();
     // thisSlide(pagIndex);
 }
+
+function activatePagination ()
+{
+    paginationBlockItem.forEach((item) =>
+    {
+        item.firstChild.classList.remove('activ-pagination');
+    })
+
+    paginationBlockItem[pagIndex].firstChild.classList.add('activ-pagination');
+}
+
+function removePausePagination()
+{
+    paginationBlockItem.forEach((item) =>
+    {
+        item.firstChild.style = '';
+    })
+}
+
+paginationBlockItem.forEach((item) =>
+{
+    item.addEventListener('animationend', () =>
+    {
+        nextSlide();
+    })
+})
 
 carouselArrowRight.addEventListener('click', nextSlide);
 carouselArrowLeft.addEventListener('click', prevSlide);
 
-setInterval(() =>
+// - - - - - -  - - - + Swipe Functional + - - - - -  - - - - 
+carouselBlocks.addEventListener('touchstart', (event) =>
 {
-    nextSlide()
-}, 
-3000);
+    swipe.posX1 = event.changedTouches[0].clientX;
+
+    const activPagination = document.querySelector('.activ-pagination');
+    activPagination.style.animationPlayState = 'paused';
+})
+
+carouselBlocks.addEventListener('touchend', (event) =>
+{
+    swipe.posX2 = event.changedTouches[0].clientX;
+    swipe.swipeAction();
+
+    removePausePagination();
+})
+
+carouselBlocks.addEventListener('mousedown', (event) =>
+{
+    swipe.posX1 = event.clientX;
+})
+
+carouselBlocks.addEventListener('mouseup', (event) =>
+{
+    swipe.posX2 = event.clientX;
+    swipe.swipeAction();
+})
+
+const swipe = 
+{
+    posX1: 0,
+    posX2: 0,
+    swipeLength: 0,
+    swipeThreshold: carouselWrapper.offsetWidth / 3,
+
+    swipeAction()
+    {
+        this.swipeLength = this.posX1 - this.posX2;
+
+        if(Math.abs(this.swipeLength) > this.swipeThreshold)
+        {
+            if(this.swipeLength > 0) nextSlide();
+            if(this.swipeLength < 0) prevSlide();
+        }
+    }
+}
+// More advanced version of the slider with swipe: https://habr.com/ru/articles/501258/
+// - - - - - -  - - - = Swipe Functional = - - - - -  - - - -
 /* ============================ -Favorite Carusel- ============================= */
 
