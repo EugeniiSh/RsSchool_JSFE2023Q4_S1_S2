8c8ab9e6cb22f87f7f10766b8c1531cd9fedd283
EugeniiSh <evgenii.shmerko@mail.ru>
2024-02-22 12:56:25 +0300

    refactor: change loader js to ts

--- news-api/src/components/view/appView.ts
+++ news-api/src/components/view/appView.ts
@@ -1,21 +1,30 @@
-import News from './news/news';
-import Sources from './sources/sources';
+import {News, NewsObject} from './news/news';
+import {Sources, SourceObject} from './sources/sources';
 
-export class AppView {
+interface ResponseData
+{
+    articles?: NewsObject[];
+    sources?: SourceObject[];
+}
+
+class AppView {
+    news: News;
+    sources: Sources;
+
     constructor() {
         this.news = new News();
         this.sources = new Sources();
     }
 
-    drawNews(data) {
+    drawNews(data: ResponseData): void {
         const values = data?.articles ? data?.articles : [];
         this.news.draw(values);
     }
 
-    drawSources(data) {
+    drawSources(data: ResponseData): void {
         const values = data?.sources ? data?.sources : [];
         this.sources.draw(values);
     }
 }
 
-export default AppView;
+export {AppView, ResponseData};
